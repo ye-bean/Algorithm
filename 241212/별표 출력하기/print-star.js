baa85a4d3f02f0c9085aa01151ed6820d0ee0350
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-12-12 16:52:30 +0900

    Title: 별표 출력하기, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241212/별표 출력하기/print-star.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+let n =Number(fs.readFileSync(0).toString().trim());
+
+for (let i = n; i > 0; i--) {      
+    str = "";
+    for (let j = 0; j <= n - i; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
+
+for (let i = 1; i < n; i++) {      
+    str = "";
+    for (let j = 0; j < n - i; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
