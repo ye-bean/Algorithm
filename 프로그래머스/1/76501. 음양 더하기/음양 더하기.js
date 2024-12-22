4f85e1ca6dc690f534ea63f9b42735d3aad3d891
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-12-22 16:39:32 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,7 @@
 function solution(absolutes, signs) {
     var answer = 0;
     for(let i = 0; i < signs.length; i++){
-        if(signs[i]===true) answer += absolutes[i];
-        else answer += -(absolutes[i]);
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i];
     }
     return answer;
 }
