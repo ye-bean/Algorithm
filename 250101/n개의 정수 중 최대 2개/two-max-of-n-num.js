545e519692ab0ec7ec8f1305465c7b920c1421a3
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2025-01-01 15:14:38 +0900

    Title: n개의 정수 중 최대 2개, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 250101/n개의 정수 중 최대 2개/two-max-of-n-num.js
+++ 250101/n개의 정수 중 최대 2개/two-max-of-n-num.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split('\n');
 
+const n = Number(input[0]);
 let arr = input[1].split(' ').map(Number).sort((a,b) => b - a);
+let num = arr[0];
+
+for(let i = 0 ; i < n ; i++){
+    for(let j = i+1; j < n; j++){
+        if(arr[i] < arr[j]) arr[i] = arr[j];
+    }
+}
 
 console.log(arr[0], arr[1]);
