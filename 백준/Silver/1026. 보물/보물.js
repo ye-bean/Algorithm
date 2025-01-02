0124706f3546d7a37fc3ef5e71eac558a05d4db8
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2025-01-03 00:27:04 +0900

    [Silver IV] Title: 보물, Time: 100 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.js
+++ 백준/Silver/1026. 보물/보물.js
@@ -5,11 +5,13 @@ const n = Number(input[0]);
 const arr1 = input[1].split(' ').map(Number).sort((a,b) => a - b);
 const arr2 = input[2].split(' ').map(Number);
 
-let newarr2 = arr2.sort((a,b) => b - a);
 let total = 0;
 
 for(let i = 0 ; i < n ; i++){
-    total += arr1[i]*newarr2[i];
+    const max = Math.max(...arr2);
+    const index = arr2.indexOf(max);
+    total += max*arr1[i];
+    arr2.splice(index, 1);
 }
 
 console.log(total);
