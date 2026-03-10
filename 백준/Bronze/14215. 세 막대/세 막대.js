d7d46b757f084a64065aa08de9ae5faf47201919
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2026-03-10 16:11:08 +0900

    [Bronze III] Title: 세 막대, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.js
+++ 백준/Bronze/14215. 세 막대/세 막대.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number).sort((a, b) => a - b);
 
-const a = input[0];
-const b = input[1];
-const c = input[2];
+const [a, b, c] = input;
 
 if (c < a + b) {
     console.log(a + b + c);
