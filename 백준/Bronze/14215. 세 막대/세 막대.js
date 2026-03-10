aaec92d80626942a5f6ff2eb4800a73b487a91ba
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2026-03-10 16:10:17 +0900

    [Bronze III] Title: 세 막대, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.js
+++ 백준/Bronze/14215. 세 막대/세 막대.js
@@ -1,30 +1,12 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number).sort((a, b) => a - b);
 
-const a = parseInt(input.shift());
-const b = parseInt(input.shift());
-const c = parseInt(input.shift());
+const a = input[0];
+const b = input[1];
+const c = input[2];
 
-let maxLen = a;
-
-if(a < b){
-    if(c < b){
-        maxLen = b;
-        if(b < a+c){console.log(a+b+c);}
-        else {console.log(a+c+a+c+-1);}
-    } else{
-        maxLen = c;
-        if(c < a+b){console.log(a+b+c);}
-        else {console.log(a+b+a+b-1);}
-    }
+if (c < a + b) {
+    console.log(a + b + c);
 } else {
-    if(c < a){
-        maxLen = a;
-        if(a < b+c){console.log(a+b+c);}
-        else {console.log(b+c+b+c-1);}
-    } else {
-        maxLen = c;
-        if(c < a+b){console.log(a+b+c);}
-        else {console.log(a+b+a+b-1);}
-    }
+    console.log((a + b) + (a + b - 1));
 }
