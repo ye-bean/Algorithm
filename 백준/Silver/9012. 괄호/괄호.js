c83633e254bebf112c7a311b9351ac3042251b90
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2026-03-23 22:16:05 +0900

    [Silver IV] Title: 괄호, Time: 96 ms, Memory: 9448 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,23 +1,31 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const n = parseInt(input[0])
+const n = parseInt(input[0]);
 let answer = [];
 
-for(let i = 1 ; i <=n ; i++){
-    let string = input[i].split('');
-    let arr = [];
-    for(let j = 0; j < string.length; j++){
-        arr.push(string[j]);
-        if (arr[0] === ')'){
-            break;
+for (let i = 1; i <= n; i++) {
+    const s = input[i].trim();
+    let stack = [];
+    let isVPS = true;
+
+    for (let char of s) {
+        if (char === '(') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0) {
+                isVPS = false;
+                break;
+            }
+            stack.pop();
         }
-        else if( (2<= arr.length) && (arr[arr.length-1] != arr[arr.length-2])){
-            arr.pop();
-            arr.pop();   
-        } 
     }
-    answer.push(arr.length === 0 ? 'YES' : 'NO');
+
+    if (isVPS && stack.length === 0) {
+        answer.push("YES");
+    } else {
+        answer.push("NO");
+    }
 }
 
 console.log(answer.join('\n'));
