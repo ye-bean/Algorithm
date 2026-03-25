ad148e1c0fee3a7e39b81d5af4b94a3da1af7cf1
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2026-03-25 22:33:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.38 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,17 @@
-function solution(participant, completion) {
-    let map = new Map();
-
+function solution(participant, completion){
+    let myMap = new Map();
+    
     for(const name of participant){
-        map.set(name, (map.get(name) || 0) + 1);
+        myMap.set(name, (myMap.get(name) || 0) + 1);
     }
     
-    for(const name of completion) {
-        map.set(name, map.get(name)-1);
+    for(const name of completion){
+        myMap.set(name, (myMap.get(name)) -1);
     }
     
-    for(const [name, count] of map){
-        if(count>0) return name;
+    for(const [name, count] of myMap){
+        if(count > 0){
+            return name;
+        }
     }
 }
