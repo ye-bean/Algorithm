698521286ca3842d73cf9ad083c89ae02338f2d7
YeBin Ahn <151003669+ye-bean@users.noreply.github.com>
2026-04-13 22:19:45 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 124 ms, Memory: 16868 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.js
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.js
@@ -1,12 +1,7 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const n = Number(input[0]);
-const arr = input[1].split(' ').map(Number);
-let mySet = new Set();
-for(const num of arr){
-    mySet.add(num);
-}
-
+let mySet = new Set(input[1].split(' ').map(Number));
 const answer = [...mySet].sort((a,b) => a - b);
 
 console.log(answer.join(' '));
